833b765942d7caeaafb01b63e7e6c17c0e462d52
saki-2525-ota <206808265+saki-2525-ota@users.noreply.github.com>
2025-11-07 12:03:02 +0900

    list

--- server.js
+++ server.js
@@ -5,5 +5,12 @@ await kv.set(['LDH', 'GENERATIONS'], { id: '1', NoP: 6 });
 await kv.set(['LDH', 'The RAMPAGE'], { id: '2', NoP: 16 });
 await kv.set(['LDH', 'FANTASTICS'], { id: '3', NoP: 8 });
 
-const LDH = await kv.get(['LDH', 'GENERATIONS']);
-console.log(LDH.key);
+//const LDH = await kv.get(['LDH', 'GENERATIONS']);
+//console.log(LDH.key);
+
+const ldh = await kv.list({ prefix: ['LDH'] });
+
+for await (const A of ldh) {
+  console.log(A.key);
+  console.log(A.value);
+}

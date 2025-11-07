97161f63718f8928c920d4a74a3605a68af865c2
saki-2525-ota <206808265+saki-2525-ota@users.noreply.github.com>
2025-11-07 12:14:47 +0900

    連番IDの生成

--- server.js
+++ server.js
@@ -15,7 +15,18 @@ for await (const A of ldh) {
   console.log(A.value);
 }
 
-const deleteList = await kv.list({ prefix: ['LDH'] });
-const atomic = kv.atomic();
-for await (const e of deleteList) atomic.delete(e.key);
-await atomic.commit();
+//const deleteList = await kv.list({ prefix: ['LDH'] });
+//const atomic = kv.atomic();
+//for await (const e of deleteList) atomic.delete(e.key);
+//await atomic.commit();
+
+async function getNextId() {
+  const key = ['counter', 'LDH'];
+  const res = await atomic().sum(key, 1n).commit();
+  if (!res.ok) {
+    console.error('IDナンバーの作成に失敗しました。');
+    return null;
+  }
+  const counter = await kv.get(key);
+  return Number(counter.value);
+}
